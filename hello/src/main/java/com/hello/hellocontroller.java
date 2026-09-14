deab216b989a98314a116c061b38c7ba4ddd2279
MANISH YADAV <my5999555@gmail.com>
2026-09-15 01:01:55 +0530

    Update hellocontroller.java

--- hello/src/main/java/com/hello/hellocontroller.java
+++ hello/src/main/java/com/hello/hellocontroller.java
@@ -1,6 +1,8 @@
 package com.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hellocontroller {
@@ -8,6 +10,9 @@ public class hellocontroller {
     public  String hello(){
         return "hello world ! ";
     }
-
+    @PostMapping("/hello")
+    public String hellopost(@RequestBody String name){
+        return "hello"+name+"!"; 
+    }
 
 }
